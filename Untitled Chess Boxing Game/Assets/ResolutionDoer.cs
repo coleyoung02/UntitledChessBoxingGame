39c9afb0a821e8f7499ff4d94b27125893e43097
coleyoung02 <coleyoung741@gmail.com>
2023-04-15 19:10:25 -0700

    resolution and pixel stuff

---
+++ Untitled Chess Boxing Game/Assets/ResolutionDoer.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class ResolutionDoer : MonoBehaviour
+{
+    private int resolutionScale = 24;
+    static int gridSize = 24;
+    private int width = 16;
+    private int height = 9;
+    [SerializeField] private TextMeshProUGUI text;
+    // Start is called before the first frame update
+    void Start()
+    {
+        int w = Display.main.systemWidth / (width * gridSize + 1);
+        Debug.Log("Width " + w);
+        int h = Display.main.systemHeight / (height * gridSize + 1);
+        Debug.Log("Height " + h);
+        resolutionScale = Math.Min(w, h);
+        if (resolutionScale <= 2) resolutionScale = 2;
+        Screen.SetResolution(width * resolutionScale * gridSize, height * resolutionScale * gridSize, false);
+        text.text = w.ToString() + ", " + h.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void changeRes(int increment)
+    {
+        if (resolutionScale + increment > 1)
+        {
+            resolutionScale += increment;
+        }
+        Screen.SetResolution(width * resolutionScale * gridSize, height * resolutionScale * gridSize, false);
+    }
+}
