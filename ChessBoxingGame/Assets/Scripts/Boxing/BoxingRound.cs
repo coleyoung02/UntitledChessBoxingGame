96d095b692f7666693ba6d17b1748d58e41271cb
EricL22 <38116692+EricL22@users.noreply.github.com>
2023-05-08 22:09:45 -0700

    Add BoxingRound

---
+++ ChessBoxingGame/Assets/Scripts/Boxing/BoxingRound.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxingRound : MonoBehaviour
+{
+    private Timer timer;
+    private PlayerFighter player;
+    private EnemyFighter enemy;
+    private int roundNumber;
+
+    void endRound()
+    {
+        
+    }
+}
