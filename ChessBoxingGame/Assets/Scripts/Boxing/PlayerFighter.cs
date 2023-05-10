2387673d85ad89af80014dfbc07c3badfa535fb7
wheezul <93493448+wheezul@users.noreply.github.com>
2023-05-09 19:45:23 -0700

    progress

---
+++ ChessBoxingGame/Assets/Scripts/Boxing/PlayerFighter.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerFighter : MonoBehaviour
+{
+   void Punch()
+    {
+        Debug.Log("punch");
+    }
+
+    void Block()
+    {
+        Debug.Log("block");
+    }
+
+    void Dodge()
+    {
+        Debug.Log("Dodge");
+    }
+}
