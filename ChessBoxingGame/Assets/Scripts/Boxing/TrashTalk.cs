8cea7e09b97e513ad0a36be1d43e9a183edb6f18
wheezul <93493448+wheezul@users.noreply.github.com>
2023-06-01 20:18:45 -0700

    word detection progbress

--- ChessBoxingGame/Assets/Scripts/Boxing/TrashTalk.cs
+++ ChessBoxingGame/Assets/Scripts/Boxing/TrashTalk.cs
@@ -8,14 +8,22 @@ public class TrashTalk : MonoBehaviour
     public InputField Talk;
     string keyword1 = "?";
     string keyword2 = "fu";
-
+    
+    
     public void Button()
     {
-        if(string.Equals(Talk.text, keyword1) || string.Equals(Talk.text, keyword2))
-        {
-            WordDetected();
-            Debug.Log("trash talk detected");
-        }
+        string playerText = Talk.text;
+        //string[] playerTextArray = playerText.Split(' ');
+        
+        //for(int i = 0; i < playerTextArray.Length; i++)
+        //{
+            if (playerText.Contains(keyword1) || playerText.Contains(keyword2))
+            {
+                WordDetected();
+                Debug.Log("trash talk detected");
+            }
+        //}
+        
     }
 
     public void WordDetected()
@@ -23,3 +31,8 @@ public class TrashTalk : MonoBehaviour
         //DamageEnemy();
     }
 }
+
+
+
+//case sensitive
+//use an array maybe
