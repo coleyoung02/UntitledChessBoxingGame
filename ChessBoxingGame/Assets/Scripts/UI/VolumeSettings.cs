b1331cbcd43fb63e2a8803467dc1c838350e3d8f
EricL22 <38116692+EricL22@users.noreply.github.com>
2023-06-03 21:33:40 -0700

    Implement Main Menu buttons

--- ChessBoxingGame/Assets/Scripts/UI/VolumeSettings.cs
+++ ChessBoxingGame/Assets/Scripts/UI/VolumeSettings.cs
@@ -10,7 +10,7 @@ public class VolumeSettings : MonoBehaviour
 
     private void Start()
     {
-        if (PlayerPrefs.HasKey("musicVolume"))
+        if (PlayerPrefs.HasKey("musicVolume") || PlayerPrefs.HasKey("SFXVolume"))
             LoadVolume();
         else
         {
