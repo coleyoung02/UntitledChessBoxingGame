92f679906a5ed814f49ca9b5363fd85bb4f7dc0e
coleyoung02 <coleyoung741@gmail.com>
2023-06-08 01:31:10 -0700

    almost done

--- ChessBoxingGame/Assets/Scripts/UI/Scroll.cs
+++ ChessBoxingGame/Assets/Scripts/UI/Scroll.cs
@@ -5,22 +5,33 @@ using UnityEngine;
 public class Scroll : MonoBehaviour
 {
     private float minY;
+    private float timePassed;
     private void Start()
     {
         minY = this.gameObject.transform.position.y;
+        timePassed = -1f;
     }
 
     void Update()
     {
         if (Input.mouseScrollDelta.y > 0)
         {
-            float y = Mathf.Max(minY, this.gameObject.transform.position.y - 1);
+            int y = (int)Mathf.Round(Mathf.Max(minY, this.gameObject.transform.position.y - 1));
             this.gameObject.transform.position = new Vector2(this.gameObject.transform.position.x, y);
+            timePassed = -1f;
         }
         else if (Input.mouseScrollDelta.y < 0)
         {
-            float y = Mathf.Min(60f+minY, this.gameObject.transform.position.y + 1);
+            int y = (int)Mathf.Round(Mathf.Min(35f+minY, this.gameObject.transform.position.y + 1));
+            this.gameObject.transform.position = new Vector2(this.gameObject.transform.position.x, y);
+            timePassed = -1f;
+        }
+        else if (timePassed > 1f)
+        {
+            timePassed = .9f;
+            float y = Mathf.Min(35f + minY, this.gameObject.transform.position.y + .125f);
             this.gameObject.transform.position = new Vector2(this.gameObject.transform.position.x, y);
         }
+        timePassed += Time.deltaTime;
     }
 }
