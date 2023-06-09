b7867df393dc9d9dc0e430fde2e5f4dbc8840bd7
big-jungus <matthewjung1010@gmail.com>
2023-06-09 12:25:36 -0700

    idk why these reverted themselves

--- ChessBoxingGame/Assets/Scripts/Boxing/Constants.cs
+++ ChessBoxingGame/Assets/Scripts/Boxing/Constants.cs
@@ -7,10 +7,10 @@ public static class Constants
         public const float BLOCKING_TIME_MIN = 1.5f;
         public const float BLOCKING_TIME_MAX = 2.5f;
         public const float LIGHT_PUNCH_TELE_TIME = 0.4f;
-        public const float LIGHT_PUNCH_DAMAGE = 3.5f; //for single round, use 10.0f
+        public const float LIGHT_PUNCH_DAMAGE = 3f; //for single round, use 10.0f
         public const float HEAVY_PUNCH_FST_TELE_TIME = 0.55f;
         public const float HEAVY_PUNCH_SND_TELE_TIME = .35f;
-        public const float HEAVY_PUNCH_DAMAGE = 7.0f; //for single round, use 15.0f
+        public const float HEAVY_PUNCH_DAMAGE = 6.0f; //for single round, use 15.0f
         public const float STUN_TIME = 1.35f;
         public const float FAKE_IDLE_TIME = .6f;
         public const float BLOCKING_REDUC = 0.8f;
@@ -29,7 +29,7 @@ public static class Constants
         public const float LIGHT_PUNCH_DAMAGE = 2.0f; //for single round, use 8.0f
         public const float HEAVY_PUNCH_TELE_TIME = .7f;
         public const float HEAVY_PUNCH_DAMAGE = 4.0f; //for single round, use 16.0f
-        public const float BLOCKING_REDUC = 0.65f;
+        public const float BLOCKING_REDUC = 0.7f;
         public const float DODGE_IMMMUNITY_TIME = 0.5f;
         public const float DODGE_NO_IMMMUNITY_TIME = 0.2f;
         public const float DODGE_DELAY = 2f;
